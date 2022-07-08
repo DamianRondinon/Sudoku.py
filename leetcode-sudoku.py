1c3f5546434fbae53f454de5d6c70df259c7afbd
DamianRondinon <95187328+DamianRondinon@users.noreply.github.com>
2022-07-08 18:54:58 -0300

    itere proyecto

--- leetcode-sudoku.py
+++ leetcode-sudoku.py
@@ -26,7 +26,7 @@ board = [
 class ValidateSudoku:
     def __init__(self,tablero) -> None:
         self.tablero = tablero
-        self.lista_invertida = list(), """Chequear que el tablero introducido sea un tablero 9x9"""
+        self.lista_invertida = list()
 
     def chequeo_general(self):
     #assert
@@ -34,22 +34,52 @@ class ValidateSudoku:
         for fila in self.tablero:
          assert len(fila) == 9, "El tablero ingresado no respeta el formato 9x9"
 
-    def chequeo_filas(self):
-        for fila in self.tablero:
+    def chequeo_filas(self,lista_a_chequear="tablero_general"):
+        if lista_a_chequear == "tablero_general":
+            lista_a_chequear = self.tablero
+       
+        for fila in lista_a_chequear:
             for elemento in fila:
                 if elemento != ".":
-                    assert fila.count(elemento) ==1, "El tablero ingresado no es válido" """Esto mismo puede hacerse en una línea conteo = fila.count(elemento) if conteo > 1 --> romper"""
+                    assert fila.count(elemento) == 1, "El tablero ingresado no es válido"
+
+    def chequeo_columnas(self):
+
+        for column_index in range(0,9):
+            for row_index in range(0,9):
+                self.lista_invertida.append(self.tablero[row_index][column_index])
+            
+
+            self.chequeo_filas([self.lista_invertida])
+
+            self.lista_invertida.clear()
+
+    def chequeo_subcuadros(self):
+        #Volvemos a dividir una gran tarea en pequeñas tareas
+        #Funcion mayor
+        #Tenemos 9 subcuadros = chequear de 3 en 3
+        #De mis primeras 3 filas -> subcuadros del 0 al 3, 3 al 6, 6 al 9
+        self.chequeo_3_subcuadros(0,3)
+        self.chequeo_3_subcuadros(3,6)
+        self.chequeo_3_subcuadros(6,9)
+
+    def chequeo_3_subcuadros(self,rango1,rango2):
+        for column_index in range(0,9):
+            if column_index == 3 or column_index == 6:
+                self.lista_invertida.clear()
+            for row_index in range(rango1,rango2):
+                self.lista_invertida.append(self.tablero[row_index][column_index])
+            self.chequeo_filas([self.lista_invertida])
+    
+
                     
-    def chequeo_filas(self):
-        for fila in self.tablero:
-            for elemento in fila:
-                if elemento != ".":
-                    assert fila.count(elemento) ==1, "El tablero ingresado no es valido" """conteo = fila.count(elemento) if conteo > 1 --> romper - utilizamos el assert"""
+
 
                     
 #instanciar el objeto
 sudoku = ValidateSudoku(board)
 sudoku.chequeo_general()
 sudoku.chequeo_filas()
-
+sudoku.chequeo_columnas()
+sudoku.chequeo_3_subcuadros()
 
